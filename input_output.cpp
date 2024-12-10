c5a036ac911bf27a7410508e8fd6f02949bf6ea4
Emrehan Gökçay <101899529+H00wb@users.noreply.github.com>
2024-12-10 08:40:00 +0300

    Create input_output.cpp

---
+++ input_output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+     int a,b,c;
+     1<=a,b,c<=1000;
+     
+     cin >> a >> b >> c;
+     
+     cout << a+b+c;
+    return 0;
+}
